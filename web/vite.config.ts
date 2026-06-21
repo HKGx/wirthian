86197ea8b16d07aeb38820221160cfadba38fcf6
HKGx <19597269+HKGx@users.noreply.github.com>
2026-06-21 23:14:36 +0200

    Use relative path for deployment

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,6 +5,7 @@ import { lezer } from "@lezer/generator/rollup";
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: "./",
     plugins: [svelte(), wasm(), lezer()],
     server: {
         fs: { allow: [".."] },
